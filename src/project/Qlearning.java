3457c9bf5abb3f805de5968a4b5701c9673e75e1
Hawkins <haakenson@gmail.com>
2017-11-29 16:41:11 -0800

    fidaddling

--- src/project/Qlearning.java
+++ src/project/Qlearning.java
@@ -1,12 +1,12 @@
 package src.project;
 
-// TODO: a li'l bit o' everything
-
 /**
- * Created by Zosit on 11/22/2017.
- * This is the Q-learning Class.
- * It should create a table of gamestates (with initial values).
- * It should create and run through a series of games to fill in the table
+ * The Qlearning class
+ * 1. Create a Qlearning object.
+ * 2. Call Qlearning.train(...).
+ * 3. Call Qlearning.test(...).
+ * 4. ???
+ * 5. Profit.
  */
 public class Qlearning {
 
@@ -15,6 +15,9 @@ public class Qlearning {
     private static int STATES  = 858 + 1; // 858 = largest legal state # possible
                                           //       Ace 21 10 [in decimal] = 1 10101 1010 [in binary]);
 
+    /**
+     * Qlearning constuctor
+     */
     public Qlearning() {
         this.q = new double[Qlearning.STATES][Qlearning.ACTIONS];
     }
@@ -29,12 +32,16 @@ public class Qlearning {
      * @param epsilonDelta Amount to change epsilon every epsilonEvery episodes
      * @param epsilonEvery Number of episodes to train before reducing epsilon by epsilonDelta
      */
-    public void train(int episodes, double eta, double gamma, double epsilon, double epsilonMin, double epsilonDelta, int epsilonEvery) {
+    public void train(int episodes, double eta, double gamma, double epsilon, double epsilonMin, double epsilonDelta, int epsilonEvery) throws IllegalArgumentException {
+        if (eta <= 0.0 || gamma <= 0.0 || epsilon <= 0.0 || epsilonMin <= 0.0 || epsilonDelta <= 0.0 ||
+            eta >= 1.0 || gamma >= 1.0 || epsilon >= 1.0 || epsilonMin >= 1.0 || epsilonDelta >= 1.0 ||
+            epsilonEvery >= episodes || epsilonMin > epsilon || episodes <= 0 || epsilonEvery < 1) {
+            throw new IllegalArgumentException("Illegal argument(s) passed to Qlearning.train()");
+        }
         int state;
         int stateNew;
         int action;
         int reward;
-        double stateAction;
 
         for (int i = 0; i < episodes; i++) { // train for #episodes
             System.out.println(i);
@@ -45,8 +52,7 @@ public class Qlearning {
                 action = getAction(state, epsilon);
                 reward = takeAction(action, game);
                 stateNew = getState(game.getPlayerHandValue(), game.isAceInPlayerHand(), game.getDealerUpCardValue());
-                stateAction = q[state][action];
-                q[state][action] = stateAction + eta * (reward + gamma * getActionMaxValue(stateNew) - stateAction);
+                q[state][action] += eta * (reward + gamma * getActionMaxValue(stateNew) - q[state][action]);
                 state = stateNew;
             } while (!game.isOver());
 
@@ -68,6 +74,9 @@ public class Qlearning {
      * @return The win percentage.
      */
     public double test(int episodes) {
+        if (episodes < 1) {
+            throw new IllegalArgumentException("Illegal argument passed to Qlearning.test()");
+        }
         int state;
         int action;
         double rewardTotal = 0.0;
@@ -86,15 +95,22 @@ public class Qlearning {
         return rewardTotal / episodes;
     }
 
+    /**
+     * Randomly play for #episodes. Return win %.
+     * @param episodes Number of episodes (games) to play.
+     * @return The win percentage.
+     */
     public double randomTest(int episodes) {
-        int action;
+        if (episodes < 1) {
+            throw new IllegalArgumentException("Illegal argument passed to Qlearning.randomTest()");
+        }
+
         double rewardTotal = 0.0;
 
         for (int i = 0; i < episodes; i++) {
             Game game = new Game();
             do {
-                action = getAction(0,1.1);
-                rewardTotal += takeAction(action, game);
+                rewardTotal += takeAction(getActionRandom(), game);
             } while (!game.isOver());
         }
 
@@ -108,6 +124,9 @@ public class Qlearning {
      * State value = 1 bit for player has Ace/no Ace
      *             + 5 bits for player hand value
      *             + 4 bits for dealer exposed card value
+     * @param playerValue The player's minimum total hand (Ace = 1).
+     * @param playerHasAce If the player has at least one Ace in hand.
+     * @param dealerValue The value of the dealer's exposed card.
      * @return The current state.
      */
     private int getState(int playerValue, boolean playerHasAce, int dealerValue) {
@@ -121,12 +140,13 @@ public class Qlearning {
 
     /**
      * Get an epsilon-greedy action to take.
-     *
+     * @param state The current state.
+     * @param epsilon The chance of selecting a random action.
      * @return The an action to take.
      */
     private int getAction(int state, double epsilon) {
         if (Math.random() < epsilon) { // return random action
-            return (int) (Math.random() * 2);
+            return getActionRandom();
         } else { // return index of max action available
             return getActionMax(state);
         }
@@ -134,7 +154,7 @@ public class Qlearning {
 
     /**
      * Get the best action to take.
-     *
+     * @param state The current state.
      * @return The best action to take.
      */
     private int getActionMax(int state) {
@@ -142,8 +162,16 @@ public class Qlearning {
     }
 
     /**
-     * Get the best available action's value.
-     *
+     * Get a random action to take.
+     * @return The random action to take.
+     */
+    private int getActionRandom() {
+        return (int) (Math.random() * Qlearning.ACTIONS);
+    }
+
+    /**
+     * Get the best value of the available actions at this state.
+     * @param state The state to observe.
      * @return The best action value.
      */
     private double getActionMaxValue(int state) {
@@ -151,8 +179,10 @@ public class Qlearning {
     }
 
     /**
-     * Take the supplied action.
-     * @return The value of the action taken.
+     * Take the supplied action, get the reward.
+     * @param action The action to take.
+     * @param game The game in which to take the action.
+     * @return The reward value of the action taken.
      */
     private int takeAction(int action, Game game) {
         if (action == 0) {
