7ed519786185ecdeb9a7671ba7d51285f4ba6d12
Hawkins <haakenson@gmail.com>
2017-12-02 20:57:25 -0800

    minor doodads

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,9 +1,7 @@
 package src.project;
 
 /**
- * Created by Zosit on 11/22/2017.
- * This class should just run the Qlearning algorithm
- * Maybe allow the input arguments to change variables like the learning rate and discount?
+ * This class handles the Qlearning object and launches the GUI.
  */
 public class Main {
 
@@ -12,7 +10,7 @@ public class Main {
 
         int episodes = 1000000;
         double eta = 0.2;
-        double gamma = 0.0005;
+        double gamma = 0.005;
         double epsilonStart = 1.0;
         double epsilonMin   = 0.1;
         double epsilonDelta = 0.01;
@@ -20,13 +18,13 @@ public class Main {
 
         algorithm.train(episodes, eta, gamma, epsilonStart, epsilonMin, epsilonDelta, epsilonEvery);
 
-        double[] winningQ = algorithm.test(1000);
-        double[] winningR = algorithm.randomTest(1000);
+        double[] winningQ = algorithm.test(10000);
+        double[] winningR = algorithm.randomTest(10000);
 
         System.out.println("----- Wins -----");
         System.out.println("Qlearn    Random");
-        System.out.printf("%5.2f%%    %5.2f%% %n", winningQ[0]*100, winningR[0]*100);
-        System.out.printf("%5.2f%%    %5.2f%% (with pushes)%n%n", winningQ[1]*100, winningR[1]*100);
+        System.out.printf("%5.2f%%    %5.2f%% (win-loss)%n", winningQ[0]*100, winningR[0]*100);
+        System.out.printf("%5.2f%%    %5.2f%% (win-push-loss)%n%n", winningQ[1]*100, winningR[1]*100);
 
         algorithm.prettyPrintQ(false);
         System.out.println("------------ Q-values ------------\n");
